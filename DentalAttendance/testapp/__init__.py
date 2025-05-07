4d6d713058cf003112f7488da11c42e53e034803
鈴木健 <suzukiken@suzukikennoMacBook-Air.local>
2025-05-07 14:48:54 +0900

    change config.py

--- DentalAttendance/testapp/__init__.py
+++ DentalAttendance/testapp/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from testapp.config import DATABASE_URI, TRACK_MODIFICATIONS, SECRET_KEY
+from testapp.config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS, SECRET_KEY
 
 #Flaskのオブジェクトを作成
 app = Flask(__name__)
 
 # 設定の読み込み
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = TRACK_MODIFICATIONS
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
 app.config['SECRET_KEY'] = SECRET_KEY
 
 # データベースの初期化
