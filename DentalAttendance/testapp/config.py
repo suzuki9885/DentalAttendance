b93b4d41463e9c1585fc47c548d1b1952acfd85e
鈴木健 <suzukiken@suzukikennoMacBook-Air.local>
2025-05-07 14:43:35 +0900

    change config.py

--- DentalAttendance/testapp/config.py
+++ DentalAttendance/testapp/config.py
@@ -7,8 +7,8 @@ ADMIN_ID = os.environ.get('ADMIN_ID')  # 環境変数から取得
 ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')  # 環境変数から取得
 
 # データベース設定
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # 環境変数から取得
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+DATABASE_URI = os.environ.get('DATABASE_URL')  # 環境変数から取得
+TRACK_MODIFICATIONS = False
 
 # セキュリティ設定
 SECRET_KEY = os.environ.get('SECRET_KEY')  # 環境変数から取得
